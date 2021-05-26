fadbf4a1e20b8d4fe671305ffd6370cfdf06aef5
Abdelrahman Mohammad Al Daour <Daour.995@outlook.com>
2021-05-26 12:43:54 +0300

    finihsed lab07 back-end

--- server.js
+++ server.js
@@ -31,21 +31,27 @@ server.get('/weather', (req, res) => {
     console.log(req.query)
     
     let cityName= req.query.city_name
-    let lonData = req.query.lon
-    let latData = req.query.lat
+    // let lonData = req.query.lon
+    // let latData = req.query.lat
     let cityData;
 
     let Items = weatherData.find(item => {
-        if (item.city_name.toLowerCase() == cityName.toLowerCase() && item.lat == latData && item.lon == lonData){
-        cityData = new Forecast(item)
+        if (item.city_name.toLowerCase() == cityName.toLowerCase() ){
             return item
         }
     })
 
-    // console.log(cityName,lonData,latData);
-    res.send(cityData)
+   try{
+       let forecastArr= Items.data.map(item =>{
+           return new Forecast(item)
+       })
+       res.send(forecastArr)
+   }
+   catch{
+       res.send('Error: The weather info for the city you searched for is not available')
+   }
 
-    // && item.lat == latData && item.lon == lonData
+    
 })
     
 
@@ -54,78 +60,14 @@ class Forecast {
     
     constructor(arrData){
         
-        this.data =arrData.data.map(item =>{
-           let low = 'low of '+item.low_temp;
-           let high = 'high of '+item.max_temp;
-
-            return {'description': low+high + 'with' + item.weather.description,'date':item.datetime}
-        })
+        this.date = arrData.valid_date
+        this.descrption = arrData.weather.description;
         // this.date = date;
         // this.description= description;
     }  
 
     
 }
-// let day1 = new Forecast(Items.data.datetime,Items.data.weather.description)
-
-
-// class Forecast {
-    
-//     constructor(date,description){
- 
-//         this.date = date;
-//         this.description= description;
-//     }
-
-
- 
-//  }
-
-// // http://localhost:3001/weather?city_name=Seattle
-// server.get('/weather',(req,res) =>{
-    
-//     console.log(req.query);
-  
-
-//     let cityNameData = req.query.city_name
-
-//     let cityItem = weatherData.find( item =>{
-//         if(item.city_name == cityNameData){
-//             return item
-//         }
-//         res.send(cityItem)
-//     })
-// })
-
-// // http://localhost:3001/weather?lon
-// server.get('/weather',(req,res) =>{
-//     console.log(req.query);
-
-//     let cityLonData = req.query.lon
-
-//     let cityItem = weatherData.find( item =>{
-//         if(item.lon == cityLonData){
-//             return item
-//         }
-//         res.send(cityItem)
-//     })
-// })
-
-// // http://localhost:3001/weather?lat
-// server.get('/weather',(req,res) =>{
-//     console.log(req.query);
-
-//     let cityLatData = req.query.lat
-
-//     let cityItem = weatherData.find( item =>{
-//         if(item.lan == cityLatData){
-//             return item
-//         }
-//         res.send(cityItem)
-//     })
-// // })
-
-
 
 
 server.get('*', (req,res) =>{
