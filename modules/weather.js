131f6ec8aac8ff21aaa3651bac25689bc0508188
Abdelrahman Mohammad Al Daour <Daour.995@outlook.com>
2021-05-27 15:53:20 +0300

    Created modules

---
+++ modules/weather.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const { default: axios } = require("axios");
+
+module.exports= getWeather;
+
+async function getWeather(city) {
+    // let city = req.query.city
+    let key = process.env.WEATHER_KEY;
+    // let key = `500b0343d93143769c99731a1b47346f`;
+
+    // test URL: https://api.weatherbit.io/v2.0/forecast/daily?city=Amman&key=500b0343d93143769c99731a1b47346f
+    let url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&key=${key}`
+
+    return axios.get(url)
+    .then(result =>{
+        const weatherArray = result.data.data.map(Item=>{
+                    return new Forecast(Item)})
+                    return weatherArray
+    })
+    
+
+
+}
+
+class Forecast {
+
+    constructor(arrData) {
+
+        this.date = arrData.valid_date
+        this.descrption = arrData.weather.description;
+        // this.date = date;
+        // this.description= description;
+    }
+}
+
--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 'use strict';
 
 const express = require('express');
-const weatherData = require('./data/weather.json');
 const cors = require('cors');
 require('dotenv').config();
-const axios = require('axios');
+// const axios = require('axios');
 
+const handleWeather = require('./modules/weather.js')
+const getMovie = require('./modules/movie.js')
+// const weatherData = require('./data/weather.json');
 const server = express();
 server.use(cors());
 
@@ -61,93 +63,71 @@ server.get('/weather', getWeather);
 server.get('/movies', getMovie);
 
 async function getWeather(req, res) {
-    // let lat = req.query.lat;
-    // let lon = req.query.lon;
-    let city = req.query.city
-    
 
-    let key = process.env.WEATHER_KEY;
+    let city = req.query.city
+    // let key = process.env.WEATHER_KEY;
     // let key = `500b0343d93143769c99731a1b47346f`;
 
     // test URL: https://api.weatherbit.io/v2.0/forecast/daily?city=Amman&key=500b0343d93143769c99731a1b47346f
-    let url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&key=${key}`
-
-
-    try {
-        const result = await axios.get(url);
-        // console.log('in axios')
-        console.log(result);
-        const weatherArray = result.data.data.map(Item=>{
-            return new Forecast(Item)
-        })
-        res.send(weatherArray);
-
-    } catch (error) {
-        // console.log(error)
-        res.status(500).send(`error in getting the weather data ==> ${error}`);
-
-    }
+    handleWeather(city)
+        .then(weatherData => res.send(weatherData))
+        .catch((error) => {
+            console.error(error);
+            res.status(500).send('Sorry. Something went wrong!')
+        });
 
 
 }
 
 
-async function getMovie(req, res) {
+// async function getMovie(req, res) {
 
-    let movie = req.query.query
+//     let movie = req.query.query
 
-    let key1 = process.env.MOVIE_API_KEY;
+//     let key1 = process.env.MOVIE_API_KEY;
 
-    // test URL: https://api.themoviedb.org/3/search/movie?api_key=436281052c009396f1046ba27be009e1&query=amman   
-    let url = `https://api.themoviedb.org/3/search/movie?api_key=${key1}&query=${movie}`
-
-    
-
-    try {
-        const movieResult = await axios.get(url);
-        const movieArray = movieResult.data.results.map(Item=>{
-            return new Movie(Item)
-        })
-        res.send(movieArray);
-
-    } catch (error) {
-        console.log(error)
-        res.status(500).send(`error in getting the movie data ==> ${error}`);
-    }
+//     // test URL: https://api.themoviedb.org/3/search/movie?api_key=436281052c009396f1046ba27be009e1&query=amman   
+//     let url = `https://api.themoviedb.org/3/search/movie?api_key=${key1}&query=${movie}`
 
 
-}
 
-class Forecast {
+//     try {
+//         const movieResult = await axios.get(url);
+//         const movieArray = movieResult.data.results.map(Item => {
+//             return new Movie(Item)
+//         })
+//         res.send(movieArray);
 
-    constructor(arrData) {
-
-        this.date = arrData.valid_date
-        this.descrption = arrData.weather.description;
-        // this.date = date;
-        // this.description= description;
-    }
-
-
-    
-}
-class Movie {
-
-    constructor(item) {
-
-        this.title = item.original_title
-        this.overview= item.overview
-        this.average_votes = item.vote_average
-        this.total_votes =item.vote_count
-        this.image_url = item.poster_path
-        this.popularity = item.popularity
-        this.released_on =item.release_date
-        
-        
-    }
+//     } catch (error) {
+//         console.log(error)
+//         res.status(500).send(`error in getting the movie data ==> ${error}`);
+//     }
 
 
-}
+// }
+
+
+
+
+
+
+// class Movie {
+
+//     constructor(item) {
+
+//         this.title = item.original_title
+//         this.overview = item.overview
+//         this.average_votes = item.vote_average
+//         this.total_votes = item.vote_count
+//         this.image_url = item.poster_path
+//         this.popularity = item.popularity
+//         this.released_on = item.release_date
+
+
+//     }
+
+
+// }
 
 
 server.get('*', (req, res) => {
